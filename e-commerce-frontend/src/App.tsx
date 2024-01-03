bc39a5b6754719bc7569756d1330898354aac19a
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-03 12:27:25 +0530

    corrected the routes

--- e-commerce-frontend/src/App.tsx
+++ e-commerce-frontend/src/App.tsx
@@ -3,12 +3,16 @@ import { lazy, Suspense } from "react";
 import Loader from "./components/loader";
 import Header from "./components/header";
 
+
+
+
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
-
-
+const Login = lazy(() => import("./pages/login"));
+const Orders = lazy(() => import("./pages/orders"));
+const OrderDetails = lazy(() => import("./pages/order-details"));
 
 
 
@@ -42,10 +46,17 @@ const App = () => {
 
 
 
+          {/* Not Logged In Route */}
+          <Route path="/login" element={<Login />} />
+
+
+
 
            {/* { Logged In User Routes} */}
            <Route>
             <Route path="/shipping" element={<Shipping />} /> 
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/orders/:id" element={<OrderDetails />} />
            </Route>
             {/* { Admin Routes} */}
           <Route>
