ad8edd1d40e8c684d53369486b5a001f530f31d8
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-04 16:22:49 +0530

    added custom error handling

---
+++ e-commerce-backend/src/utils/utility-class.ts
@@ -0,0 +1,9 @@
+
+class ErrorHandler extends Error {
+  constructor(public message: string, public statusCode: number) {
+    super(message)
+    this.statusCode = statusCode;
+  }
+}
+
+export default ErrorHandler;
