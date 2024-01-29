36d6d61e30f3f5ba8cc70906c1c765b396a7a850
Ayush Shukla <123shuklaayush@gmail.com>
2024-01-29 14:20:58 +0530

    addedd github link

--- e-commerce-frontend/src/components/header.tsx
+++ e-commerce-frontend/src/components/header.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
+  FaCode,
   FaSearch,
   FaShoppingBag,
   FaSignInAlt,
@@ -12,26 +13,27 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import toast from "react-hot-toast";
 
-
-interface PropsType{
+interface PropsType {
   user: User | null;
 }
 
-const Header = ({ user } : PropsType) => {
-    const logoutHandler = async () => {
-      try{
-        await signOut(auth);
-        toast.success("Sign Out Successfully")
-        setIsOpen(false)
-      }
-      catch(err){
-        toast.error("Something went wrong")
-      }
+const Header = ({ user }: PropsType) => {
+  const logoutHandler = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Sign Out Successfully");
+      setIsOpen(false);
+    } catch (err) {
+      toast.error("Something went wrong");
     }
-    const [isOpen, setIsOpen] = useState<boolean>(false) ;
+  };
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <nav className="header" >
-      <Link onClick={() => setIsOpen(false)} to={"/"}> HOME </Link>
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        {" "}
+        HOME{" "}
+      </Link>
       <Link onClick={() => setIsOpen(false)} to={"/search"}>
         {" "}
         <FaSearch />{" "}
@@ -51,7 +53,9 @@ const Header = ({ user } : PropsType) => {
               {user.role === "admin" ? (
                 <Link to="/admin/dashboard">Admin</Link>
               ) : null}
-              <Link onClick={() => setIsOpen(false)} to="/orders">Orders</Link>
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
               <button onClick={logoutHandler}>
                 <FaSignOutAlt />
               </button>
@@ -63,6 +67,16 @@ const Header = ({ user } : PropsType) => {
           <FaSignInAlt />
         </Link>
       )}
+      <a
+        href="https://github.com/123shuklaayush/E-commerce-Website"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-link"
+      >
+        <span className="rounded-full bg-slate-700 p-2">
+          <FaCode className=" text-slate-200" />
+        </span>
+      </a>
     </nav>
   );
 };
